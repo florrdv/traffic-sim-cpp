b35ed0993e45c02656a85c735410925c97fb5ebd
Florrdv <flor@ronsmans.me>
2022-03-03 16:05:19 +0100

    Reworked errors

--- src/util/XMLParser.cc
+++ src/util/XMLParser.cc
@@ -6,20 +6,18 @@ void XMLParser::parse() {
     pugi::xml_document doc;
     pugi::xml_parse_result result = doc.load_file("input/example.xml");
 
-    if (!result) throw "XML: Invalid XML structure";
+    if (!result) throw std::runtime_error("XML: invalid file");
 
     for (pugi::xml_node tool : doc) {
         std::string name = tool.name();
         if (name == "BAAN") {
             std::cout << "Found road" << std::endl;
-        }
-        else if (name == "VERKEERSLICHT") {
+        } else if (name == "VERKEERSLICHT") {
             std::cout << "Found traffic light" << std::endl;
-        }
-        else if (name == "VOERTUIG") {
+        } else if (name == "VOERTUIG") {
             std::cout << "Found vehicle" << std::endl;
         } else {
-            throw "XML: Unknown tag '" + name + "'";
+            throw std::runtime_error("XML: Unknown tag '" + name + "'");
         }
     }
 
