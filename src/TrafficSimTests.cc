47ff650509fb8ad23e438ce6a884390e28127b41
Florrdv <flor@ronsmans.me>
2022-03-03 16:30:18 +0100

    Added headers

---
+++ src/TrafficSimTests.cc
@@ -0,0 +1,9 @@
+/*
+ * Project: PSE Traffic Simulator
+ * Author: Flor Ronsmans De Vry (flor.ronsmansdevry@student.uantwerpen.be), Achraf Yandouzi (achraf.yandouzi@student.uantwerpen.be)
+ * Description: tests entrypoint
+ * Version: 1.0
+ * License: Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+ * -----
+ * File Created: Thursday, 3rd March 2022 2:17:51 pm
+ */
