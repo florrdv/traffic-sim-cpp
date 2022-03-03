37167dfcf936bf6c817f0e709afb5fc14a357f61
YAchrafY <achraf.yandouzi@gmail.com>
2022-03-03 15:05:09 +0100

    Working on Parser

--- src/util/XMLParser.cc
+++ src/util/XMLParser.cc
@@ -7,4 +7,12 @@ void XMLParser::parse() {
         std::cerr << doc.ErrorDesc() << std::endl;
         return;
     }
+    TiXmlElement* root = doc.FirstChildElement();
+    if (root == NULL) {
+        std::cerr << "Could not load XML file: No root element found." << std::endl;
+        doc.Clear();
+        return;
+    }
+    
+
 }
