46f94285242228456c82966b27b2d496de63e2c0
Florrdv <flor@ronsmans.me>
2022-03-09 12:49:35 +0100

    Re-positioned header

--- src/util/XMLParser.h
+++ src/util/XMLParser.h
@@ -1,14 +1,3 @@
-//============================================================================
-// Name        : XMLParser.cc
-// Author      : Achraf Yandouzi, Flor Ronsmans De Vry
-// Description : XML Parser
-//============================================================================
-
-#include "../Simulation.h"
-
-#ifndef __PROJECTS_PSE_TRAFFIC_SIM_SRC_UTIL_XMLPARSER_H_
-#define __PROJECTS_PSE_TRAFFIC_SIM_SRC_UTIL_XMLPARSER_H_
-
 /*
  * Project: PSE Traffic Simulator
  * Author: Flor Ronsmans De Vry (flor.ronsmansdevry@student.uantwerpen.be), Achraf Yandouzi (achraf.yandouzi@student.uantwerpen.be)
@@ -18,6 +7,11 @@
  * -----
  * File Created: Thursday, 3rd March 2022 3:17:49 pm
  */
+
+#include "../Simulation.h"
+
+#ifndef __PROJECTS_PSE_TRAFFIC_SIM_SRC_UTIL_XMLPARSER_H_
+#define __PROJECTS_PSE_TRAFFIC_SIM_SRC_UTIL_XMLPARSER_H_
 
 
 #include <iostream>
