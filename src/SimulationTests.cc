a3af0ac842b75c445e61fd4bc759aa361d7a3a7c
Florrdv <flor@ronsmans.me>
2022-03-21 13:19:34 +0100

    Added out folder

--- src/SimulationTests.cc
+++ src/SimulationTests.cc
@@ -7,6 +7,10 @@ TEST(SimulationTests, Temp) {
     EXPECT_EQ(5, 5);
 }
 
-TEST(SimulationTests, FileTestExample) {
+TEST(SimulationTests, FileTestExample1) {
+    EXPECT_TRUE(fileCompare(TEST_INPUT_FOLDER + "/test1.txt", TEST_INPUT_FOLDER + "/test2.txt"));
+}
+
+TEST(SimulationTests, FileTestExample2) {
     EXPECT_TRUE(fileCompare(TEST_INPUT_FOLDER + "/test1.txt", TEST_INPUT_FOLDER + "/test2.txt"));
 }
