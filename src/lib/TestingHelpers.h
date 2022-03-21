e2edc26a2b7ea5ae2ffc43fc0a7535759bffc84d
Florrdv <flor@ronsmans.me>
2022-03-21 13:10:38 +0100

    Added TestingHelpers.h / .cc

---
+++ src/lib/TestingHelpers.h
@@ -0,0 +1,8 @@
+#ifndef __PROJECTS_PSE_TRAFFIC_SIM_SRC_LIB_TESTINGHELPERS_H_
+#define __PROJECTS_PSE_TRAFFIC_SIM_SRC_LIB_TESTINGHELPERS_H_
+
+#include <string>
+
+bool FileCompare(const std::string leftFileName, const std::string rightFileName);
+
+#endif // __PROJECTS_PSE_TRAFFIC_SIM_SRC_LIB_TESTINGHELPERS_H_
