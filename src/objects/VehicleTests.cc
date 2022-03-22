f4b4cd1950d57344a1187025800a97aa62ac8bce
Florrdv <flor@ronsmans.me>
2022-03-22 16:51:41 +0100

    Started working on VehicleTests

--- src/objects/VehicleTests.cc
+++ src/objects/VehicleTests.cc
@@ -1,5 +1,25 @@
 #include <gtest/gtest.h>
+#include "Vehicle.h"
 
-TEST(VehicleTests, Temp) {
-    EXPECT_EQ(5, 5);
+TEST(VehicleTests, PositionMutationHappyDay) {
+    Vehicle v = Vehicle();
+    v.setPosition(1.0);
+    EXPECT_EQ(v.getPosition(), 1.0);
+}
+
+TEST(VehicleTests, PositionMutationNegative) {
+    Vehicle v = Vehicle();
+    EXPECT_DEATH(v.setPosition(-1), "negative");
+}
+
+TEST(VehicleTests, TickHappyDay) {
+    Vehicle v = Vehicle();
+
+    // Tick twice
+    v.tick(nullptr);
+    v.tick(nullptr);
+
+    EXPECT_TRUE(std::abs(v.getSpeed() - 0.023903999999999998) < 0.00001);
+    EXPECT_TRUE(std::abs(v.getAcceleration() - 1.4399999999938082) < 0.00001);
+    EXPECT_TRUE(std::abs(v.getPosition() - 0.00059520959999999996) < 0.00001);
 }
