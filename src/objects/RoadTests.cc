5b25761f407018d98a20e90342be495c74bf0184
Florrdv <flor@ronsmans.me>
2022-03-22 20:09:01 +0100

    Renamed Road tests

--- src/objects/RoadTests.cc
+++ src/objects/RoadTests.cc
@@ -1,32 +1,32 @@
 #include <gtest/gtest.h>
 #include "Road.h"
 
-TEST(RoadTests, roadLengthZero) {
-    Road road = Road();
-    EXPECT_DEATH(road.setLength(0), "strictly positive");
-}
-
-TEST(RoadTests, roadLengthNegative) {
-    Road road = Road();
-
-    EXPECT_DEATH(road.setLength(-20), "strictly positive");
-}
-
-TEST(RoadTests, getSetName) {
-    Road road = Road();
-
-    road.setName("test");
-    ASSERT_EQ("test", road.getName());
-}
-
-TEST(RoadTests, getSetLength) {
+TEST(RoadTests, LengthMutationHappyDay) {
     Road road = Road();
 
     road.setLength(5);
     ASSERT_EQ(5, road.getLength());
 }
 
-TEST(RoadTests, getSetVehicle) {
+TEST(RoadTests, LengthMutationZero) {
+    Road road = Road();
+    EXPECT_DEATH(road.setLength(0), "strictly positive");
+}
+
+TEST(RoadTests, LengthMutationNegative) {
+    Road road = Road();
+
+    EXPECT_DEATH(road.setLength(-20), "strictly positive");
+}
+
+TEST(RoadTests, NameMutationHappyDay) {
+    Road road = Road();
+
+    road.setName("test");
+    ASSERT_EQ("test", road.getName());
+}
+
+TEST(RoadTests, VehiclesMutationHappyDay) {
     Road road = Road();
 
     Vehicle* v = new Vehicle();
@@ -34,7 +34,7 @@ TEST(RoadTests, getSetVehicle) {
     ASSERT_EQ(v, road.getVehicles()[0]);
 }
 
-TEST(RoadTests, getSetGenerator) {
+TEST(RoadTests, GeneratorMutationHappyDay) {
     Road road = Road();
 
     VehicleGenerator* g = new VehicleGenerator();
