00e8be18e68d21e1e686c66e6939a3d9b6fa93b4
Florrdv <flor@ronsmans.me>
2022-03-22 14:54:36 +0100

    Added zero checks

--- src/util/XMLParser.cc
+++ src/util/XMLParser.cc
@@ -90,6 +90,7 @@ void XMLParser::parse(Simulation& sim, const std::string file) {
             // Extract values
             std::string roadName = nameNode.text().as_string();
             int roadLength = parsePositiveInteger(lengthNode.text().as_string(), "length");
+            if (roadLength == 0) throw std::runtime_error("XML: road length must be strictly positive");
 
             // Create road object
             Road* road = new Road();
@@ -114,6 +115,7 @@ void XMLParser::parse(Simulation& sim, const std::string file) {
             std::string road = roadNode.text().as_string();
             int pos = parsePositiveInteger(positionNode.text().as_string(), "position");
             int cycle = parsePositiveInteger(cycleNode.text().as_string(), "cycle");
+            if (cycle == 0) throw std::runtime_error("XML: cycle must be strictly positive");
 
             // Create traffic light object
             TrafficLight* trafficLight = new TrafficLight();
@@ -157,6 +159,7 @@ void XMLParser::parse(Simulation& sim, const std::string file) {
             // Extract values
             std::string road = roadNode.text().as_string();
             int freq = parsePositiveInteger(freqNode.text().as_string(), "frequency");
+            if (freq == 0) throw std::runtime_error("XML: frequency must be strictly positive");
 
             // Create vehicle generator object
             VehicleGenerator* generator = new VehicleGenerator();
