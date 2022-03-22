234f86a84a90151e48edb2f09830b97636f94c10
YAchrafY <achraf.yandouzi@gmail.com>
2022-03-22 16:35:34 +0100

    Write road tests

--- src/objects/RoadTests.cc
+++ src/objects/RoadTests.cc
@@ -1,5 +1,57 @@
 #include <gtest/gtest.h>
+#include "Road.h"
 
-TEST(RoadTests, Temp) {
-    EXPECT_EQ(5, 5);
+TEST(RoadTests, roadLengthZero) {
+    Road road = Road();
+
+    road.setLength(0);
 }
+
+TEST(RoadTests, roadLengthNegative) {
+    Road road = Road();
+
+    road.setLength(-20);
+}
+
+TEST(RoadTests, multipleGenerators) {
+    Road road = Road();
+
+    VehicleGenerator g1;
+    VehicleGenerator g2;
+    road.addGenerator(&g1);
+    road.addGenerator(&g2);
+}
+
+TEST(RoadTests, getSetName) {
+    Road road = Road();
+
+    road.setName("test");
+    ASSERT_EQ("test", road.getName());
+}
+
+TEST(RoadTests, getSetLength) {
+    Road road = Road();
+
+    road.setLength(5);
+    ASSERT_EQ(5, road.getLength());
+}
+
+TEST(RoadTests, getSetVehicle) {
+    Road road = Road();
+
+    Vehicle v;
+    road.addVehicle(&v);
+    ASSERT_EQ(&v, road.getVehicles()[0]);
+}
+
+TEST(RoadTests, getSetGenerator) {
+    Road road = Road();
+
+    VehicleGenerator g;
+    road.addGenerator(&g);
+    ASSERT_EQ(&g, road.getGenerators()[0]);
+}
+
+
+
+
