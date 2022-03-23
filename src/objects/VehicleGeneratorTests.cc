79377e8752b4ae11436f935a476822696a552537
Florrdv <flor@ronsmans.me>
2022-03-23 01:19:23 +0100

    Renamed all vehicle generator tests

--- src/objects/VehicleGeneratorTests.cc
+++ src/objects/VehicleGeneratorTests.cc
@@ -1,19 +1,19 @@
 #include <gtest/gtest.h>
 #include "VehicleGenerator.h"
 
-TEST(GeneratorTests, setFrequency1) {
+TEST(GeneratorTests, FrequencyMutationNegative) {
     VehicleGenerator* g = new VehicleGenerator();
 
     EXPECT_DEATH(g->setFrequency(-20), "strictly positive");
 }
 
-TEST(GeneratorTests, setFrequency2) {
+TEST(GeneratorTests, FrequencyMutationZero) {
     VehicleGenerator* g = new VehicleGenerator();
 
     EXPECT_DEATH(g->setFrequency(0), "strictly positive");
 }
 
-TEST(GeneratorTests, getSetFrequency) {
+TEST(GeneratorTests, FrequencyMutationHappyDay) {
     VehicleGenerator* g = new VehicleGenerator();
     g->setFrequency(20);
     EXPECT_EQ(20, g->getFrequency());
