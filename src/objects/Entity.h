337ff14d55f61c1dbdf09333076efc5653181469
Florrdv <flor@ronsmans.me>
2022-03-23 15:11:42 +0100

    Started working on entity logic

--- src/objects/Entity.h
+++ src/objects/Entity.h
@@ -2,7 +2,26 @@
 #define __PROJECTS_PSE_TRAFFIC_SIM_SRC_OBJECTS_ENTITY_H_
 
 class Entity {
+private:
+    Entity* _init;
 
+    double position{};
+
+protected:
+    // Constructors / destructors
+
+    // Regular methods
+    /**
+    \n REQUIRE(this->properlyInitialized(), "Entity wasn't initialized properly");
+    */
+    void setPosition(double p);
+     /**
+    \n REQUIRE(this->properlyInitialized(), "Entity wasn't initialized properly");
+    */
+    double getPosition() const;
+
+    // Safety specific
+    bool properlyInitialized() const { return _init == this; }
 };
 
 #endif // __PROJECTS_PSE_TRAFFIC_SIM_SRC_OBJECTS_ENTITY_H_
