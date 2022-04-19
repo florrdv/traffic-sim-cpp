ad9d714a9831432043253440e482626f436cc2c4
YAchrafY <achraf.yandouzi@gmail.com>
2022-04-19 22:30:47 +0200

    Add "in-between checks" TrafficLightTests.cc

--- src/objects/TrafficLightTests.cc
+++ src/objects/TrafficLightTests.cc
@@ -3,31 +3,33 @@
 
 TEST(TrafficLightTests, PositionMutationHappyDay) {
     TrafficLight light = TrafficLight(0.0, 10);
+    EXPECT_EQ(0, light.getPosition());
     light.setPosition(20);
     EXPECT_EQ(20, light.getPosition());
 }
 
 TEST(TrafficLightTests, PositionMutationNegative) {
     TrafficLight light = TrafficLight(0.0, 10);
-
+    EXPECT_EQ(0, light.getPosition());
     EXPECT_DEATH(light.setPosition(-20), "positive");
 }
 
 TEST(TrafficLightTests, CycleMutationHappyDay) {
     TrafficLight light = TrafficLight(0.0, 10);
+    EXPECT_EQ(10, light.getCycle());
     light.setCycle(20);
     EXPECT_EQ(20, light.getCycle());
 }
 
 TEST(TrafficLightTests, CycleMutationZero) {
     TrafficLight light = TrafficLight(0.0, 10);
-
+    EXPECT_EQ(10, light.getCycle());
     EXPECT_DEATH(light.setCycle(0), "strictly positive");
 }
 
 TEST(TrafficLightTests, CycleMutationNegative) {
     TrafficLight light = TrafficLight(0.0, 10);
-
+    EXPECT_EQ(10, light.getCycle());
     EXPECT_DEATH(light.setCycle(-20), "strictly positive");
 }
 
@@ -40,13 +42,13 @@ TEST(TrafficLightTests, ToggleHappyDay) {
 
 TEST(TrafficLightTests, CycleCountMutationHappyDay) {
     TrafficLight light = TrafficLight(0.0, 10);
-
+    EXPECT_EQ(10, light.getCycle());
     light.setCycleCount(0);
     ASSERT_EQ(0, light.getCycleCount());
 }
 
 TEST(TrafficLightTests, CycleCountMutationNegative) {
     TrafficLight light = TrafficLight(0.0, 10);
-
+    EXPECT_EQ(10, light.getCycle());
     EXPECT_DEATH(light.setCycleCount(-20), "positive");
 }
