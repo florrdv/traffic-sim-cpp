0028c2b83aafd59b2be39c65378de6b0a5b77e53
YAchrafY <achraf.yandouzi@gmail.com>
2022-04-19 22:38:38 +0200

    Add "in-between checks" VehicleGeneratorTests.cc

--- src/objects/VehicleGeneratorTests.cc
+++ src/objects/VehicleGeneratorTests.cc
@@ -3,31 +3,32 @@
 
 TEST(GeneratorTests, FrequencyMutationNegative) {
     VehicleGenerator* g = new VehicleGenerator(0.0);
-
+    EXPECT_EQ(0.0, g->getFrequency());
     EXPECT_DEATH(g->setFrequency(-20), "strictly positive");
 }
 
 TEST(GeneratorTests, FrequencyMutationZero) {
     VehicleGenerator* g = new VehicleGenerator(0.0);
-
+    EXPECT_EQ(0.0, g->getFrequency());
     EXPECT_DEATH(g->setFrequency(0), "strictly positive");
 }
 
 TEST(GeneratorTests, FrequencyMutationHappyDay) {
     VehicleGenerator* g = new VehicleGenerator(0.0);
+    EXPECT_EQ(0.0, g->getFrequency());
     g->setFrequency(20);
     EXPECT_EQ(20, g->getFrequency());
 }
 
 TEST(GeneratorTests, FrequencyCountMutationNegative) {
     VehicleGenerator* g = new VehicleGenerator(0.0);
-
+    EXPECT_EQ(0.0, g->getFrequency());
     EXPECT_DEATH(g->setFrequencyCount(-20), "positive");
 }
 
 TEST(GeneratorTests, FrequencyCountMutationHappyDay) {
     VehicleGenerator* g = new VehicleGenerator(0.0);
-
+    EXPECT_EQ(0.0, g->getFrequency());
     g->setFrequencyCount(0);
     ASSERT_EQ(0, g->getFrequencyCount());
 }
