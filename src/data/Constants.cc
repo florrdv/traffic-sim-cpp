0ff06a367a053bdff9be910687a75c1b31445395
Florrdv <flor@ronsmans.me>
2022-04-23 22:48:36 +0200

    Started cleanup

--- src/data/Constants.cc
+++ src/data/Constants.cc
@@ -53,12 +53,6 @@ std::map<VehicleType, VehicleConstant> gVehicleConstants = {
     }
 };
 
-double gVehicleLength           = 4;
-double gSpeedMax                = 16.6;
-double gAccelerationMax         = 1.44;
-double gBrakeMax                = 4.61;
-double gBrakeDistance           = 15;
-double gFollowMin               = 4;
 double gSimTime                 = 0.0166;
 double gDecelerationDistance    = 50;
 double gDecelerationFactor      = 0.4;
