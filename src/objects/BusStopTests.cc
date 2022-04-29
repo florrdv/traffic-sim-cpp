f324d6ccafbbe2d5fc615f2a60600dace20e3819
YAchrafY <achraf.yandouzi@gmail.com>
2022-04-30 00:19:48 +0200

    Write BusStop tests

---
+++ src/objects/BusStopTests.cc
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include <fstream>
+
+#include "../data/Constants.h"
+#include "../lib/TestingHelpers.h"
+#include "../objects/BusStop.h"
+
+
+TEST(BusStopTests, PositionMutationHappyDay) {
+    BusStop busStop = BusStop(0.0, 10);
+    EXPECT_EQ(0, busStop.getPosition());
+    busStop.setPosition(20);
+    EXPECT_EQ(20, busStop.getPosition());
+}
+
+TEST(BusStopTests, PositionMutationNegative) {
+    BusStop busStop = BusStop(0.0, 10);
+    EXPECT_EQ(0, busStop.getPosition());
+    EXPECT_DEATH(busStop.setPosition(-20), "positive");
+}
+
+TEST(BusStopTests, WaitTimeMutationHappyDay) {
+    BusStop busStop = BusStop(0.0, 10);
+    EXPECT_EQ(10, busStop.getWaitTime());
+    busStop.setWaitTime(20);
+    EXPECT_EQ(20, busStop.getWaitTime());
+}
+
+TEST(BusStopTests, WaitTimeMutationZero) {
+    BusStop busStop = BusStop(0.0, 10);
+    EXPECT_EQ(10, busStop.getWaitTime());
+    EXPECT_DEATH(busStop.setWaitTime(0), "strictly positive");
+}
+
+TEST(BusStopTests, WaitTimeMutationNegative) {
+    BusStop busStop = BusStop(0.0, 10);
+    EXPECT_EQ(10, busStop.getWaitTime());
+    EXPECT_DEATH(busStop.setWaitTime(-20), "strictly positive");
+}
+
+TEST(BusStopTests, TimeCountMutationHappyDay) {
+    BusStop busStop = BusStop(0.0, 10);
+    EXPECT_EQ(0, busStop.getTimeCount());
+    busStop.setTimeCount(10);
+    EXPECT_EQ(10, busStop.getTimeCount());
+}
+
+TEST(BusStopTests, TimeCountMutationNegative) {
+    BusStop busStop = BusStop(0.0, 10);
+    EXPECT_EQ(0, busStop.getTimeCount());
+    EXPECT_DEATH(busStop.setTimeCount(-20), "positive");
+}
