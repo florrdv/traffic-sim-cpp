15d6fc0e8de6d9627daa328f1932037612a8d758
YAchrafY <achraf.yandouzi@gmail.com>
2022-05-02 11:01:53 +0200

    Remove condition comments from Entity.cc

--- src/objects/Entity.cc
+++ src/objects/Entity.cc
@@ -8,18 +8,11 @@ Entity::Entity(double p) {
     _init = this;
 }
 
-/**
-\n REQUIRE(this->properlyInitialized(), "Entity was not properly initialized");
-*/
 double Entity::getPosition() const {
     REQUIRE(this->properlyInitialized(), "Entity was not properly initialized");
     return position;
 }
 
-/**
-\n REQUIRE(this->properlyInitialized(), "Vehicle was not properly initialized");
-\n ENSURE(p>=0, "Entity position cannot be a negative integer");
-*/
 void Entity::setPosition(double p) {
     REQUIRE(this->properlyInitialized(), "Vehicle was not properly initialized");
     ENSURE(p>=0, "Entity position must be a positive double");
