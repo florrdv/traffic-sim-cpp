74238906a6c5766cb6e24d87255a8c162dba557f
Florrdv <flor@ronsmans.me>
2022-05-03 19:11:42 +0200

    Fixed memory issues

--- src/util/XMLParser.cc
+++ src/util/XMLParser.cc
@@ -381,7 +381,7 @@ void XMLParser::parse(Simulation &sim, const std::string file) {
         CrossroadDetails* details2 = details[1];
         Crossroad* crossroadParsed = new Crossroad(details1, details2);
         details1->road->addCrossroad(crossroadParsed);
-        details2->road->addCrossroad(crossroadParsed);
+        details2->road->addCrossroad(crossroadParsed->clone());
     }
 }
 
