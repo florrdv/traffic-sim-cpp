2e168ed0da2558a945fec019da85c2d872a32f19
Florrdv <flor@ronsmans.me>
2022-05-03 17:10:59 +0200

    Rewrote crossroad tests

--- src/objects/CrossroadTests.cc
+++ src/objects/CrossroadTests.cc
@@ -10,42 +10,42 @@
 TEST(CrossroadTests, CrossroadGenerationHappyDay) {
     Road *road1 = new Road("foo", 100);
     Road *road2 = new Road("bar", 80);
-    EXPECT_EXIT({Crossroad(CrossroadDetails { road1, 10 }, CrossroadDetails { road2, 5 }); fprintf(stderr, "Done"); exit(0);},
+    EXPECT_EXIT({Crossroad(new CrossroadDetails { road1, 10 }, new CrossroadDetails { road2, 5 }); fprintf(stderr, "Done"); exit(0);},
                 ::testing::ExitedWithCode(0), "Done");
 }
 
 TEST(CrossroadTests, CrossroadGenerationNegativePosition1) {
     Road *road1 = new Road("foo", 100);
     Road *road2 = new Road("bar", 80);
-    EXPECT_DEATH(Crossroad(CrossroadDetails { road1, -10 }, CrossroadDetails { road2, 5 }), "Position must be positive");
+    EXPECT_DEATH(Crossroad(new CrossroadDetails { road1, -10 }, new CrossroadDetails { road2, 5 }), "Position must be positive");
 }
 
 TEST(CrossroadTests, CrossroadGenerationNegativePosition2) {
     Road *road1 = new Road("foo", 100);
     Road *road2 = new Road("bar", 80);
-    EXPECT_DEATH(Crossroad(CrossroadDetails { road1, 10 }, CrossroadDetails { road2, -20 }), "Position must be positive");
+    EXPECT_DEATH(Crossroad(new CrossroadDetails { road1, 10 }, new CrossroadDetails { road2, -20 }), "Position must be positive");
 }
 
 TEST(CrossroadTests, CrossroadGenerationInvalidPosition1) {
     Road *road1 = new Road("foo", 100);
     Road *road2 = new Road("bar", 80);
-    EXPECT_DEATH(Crossroad(CrossroadDetails { road1, 110 }, CrossroadDetails { road2, 5 }), "Position must be within road bounds");
+    EXPECT_DEATH(Crossroad(new CrossroadDetails { road1, 110 }, new CrossroadDetails { road2, 5 }), "Position must be within road bounds");
 }
 
 TEST(CrossroadTests, CrossroadGenerationInvalidPosition2) {
     Road *road1 = new Road("foo", 100);
     Road *road2 = new Road("bar", 80);
-    EXPECT_DEATH(Crossroad(CrossroadDetails { road1, 10 }, CrossroadDetails { road2, 90 }), "Position must be within road bounds");
+    EXPECT_DEATH(Crossroad(new CrossroadDetails { road1, 10 }, new CrossroadDetails { road2, 90 }), "Position must be within road bounds");
 }
 
 TEST(CrossroadTests, CrossroadGenerationInvalidRoad1) {
     Road *road1 = nullptr;
     Road *road2 = new Road("bar", 80);
-    EXPECT_DEATH(Crossroad(CrossroadDetails { road1, 10 }, CrossroadDetails { road2, 5 }), "Road can not be nullptr");
+    EXPECT_DEATH(Crossroad(new CrossroadDetails { road1, 10 }, new CrossroadDetails { road2, 5 }), "Road can not be nullptr");
 }
 
 TEST(CrossroadTests, CrossroadGenerationInvalidRoad2) {
     Road *road1 = new Road("foo", 100);
     Road *road2 = nullptr;
-    EXPECT_DEATH(Crossroad(CrossroadDetails { road1, 10 }, CrossroadDetails { road2, 5 }), "Road can not be nullptr");
+    EXPECT_DEATH(Crossroad(new CrossroadDetails { road1, 10 }, new CrossroadDetails { road2, 5 }), "Road can not be nullptr");
 }
