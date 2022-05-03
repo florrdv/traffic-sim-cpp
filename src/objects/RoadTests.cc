17aa91af8fae4c516b25a0bdddb540e95a1f67ad
Florrdv <flor@ronsmans.me>
2022-05-03 19:24:29 +0200

    Added GetLeadingVehicleUnknown

--- src/objects/RoadTests.cc
+++ src/objects/RoadTests.cc
@@ -243,9 +243,6 @@ TEST(RoadTests, GetLeadingVehicleInvalid) {
 TEST(RoadTests, GetLeadingVehicleUnknown) {
     Road road = Road("example", 100.0);
     Vehicle* vehicle1 = new Vehicle(10, VehicleType::Personal);
-    Vehicle* vehicle2 = new Vehicle(20, VehicleType::Personal);
-    road.addVehicle(vehicle1);
-    road.addVehicle(vehicle2);
 
-    EXPECT_EQ(road.getLeadingVehicle(vehicle1), vehicle2);
+    EXPECT_DEATH(road.getLeadingVehicle(vehicle1), "Vehicle must be on road");
 }
