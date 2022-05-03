75acc2f6191a0d59a2faece23cab7d5dfa04aa4d
Florrdv <flor@ronsmans.me>
2022-05-03 19:23:30 +0200

    Started working on GetLeadingVehicleInvalid

--- src/objects/RoadTests.cc
+++ src/objects/RoadTests.cc
@@ -233,3 +233,23 @@ TEST(RoadTests, GetLeadingVehicleHappyDay) {
 
     EXPECT_EQ(road.getLeadingVehicle(vehicle1), vehicle2);
 }
+
+TEST(RoadTests, GetLeadingVehicleInvalid) {
+    Road road = Road("example", 100.0);
+    Vehicle* vehicle1 = new Vehicle(10, VehicleType::Personal);
+    Vehicle* vehicle2 = new Vehicle(20, VehicleType::Personal);
+    road.addVehicle(vehicle1);
+    road.addVehicle(vehicle2);
+
+    EXPECT_EQ(road.getLeadingVehicle(vehicle1), vehicle2);
+}
+
+TEST(RoadTests, GetLeadingVehicleUnknown) {
+    Road road = Road("example", 100.0);
+    Vehicle* vehicle1 = new Vehicle(10, VehicleType::Personal);
+    Vehicle* vehicle2 = new Vehicle(20, VehicleType::Personal);
+    road.addVehicle(vehicle1);
+    road.addVehicle(vehicle2);
+
+    EXPECT_EQ(road.getLeadingVehicle(vehicle1), vehicle2);
+}
